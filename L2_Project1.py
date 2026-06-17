e983167f082ed55ff7b4b435aea4983e02cc40c4
rheapinto09 <rheaanjalipinto@gmail.com>
2026-06-17 12:42:13 +0530

    Add files via upload

---
+++ L2_Project1.py
@@ -0,0 +1,11 @@
+raj_friend1 = "12 January"
+raj_friend2 = "5 March"
+raj_friend3 = "18 April"
+raj_friend4 = "22 July"
+raj_friend5 = "30 December"
+
+print("Aman's birthday:", raj_friend1)
+print("Liam's birthday:", raj_friend2)
+print("Tom's birthday:", raj_friend3)
+print("Katie's birthday:", raj_friend4)
+print("Bruno's birthday:", raj_friend5)

9e35e32500d95310479ad34472764a11b40d8657
rheapinto09 <rheaanjalipinto@gmail.com>
2026-06-17 12:16:51 +0530

    Add files via upload

---
+++ L2_print_statement.py
@@ -0,0 +1,10 @@
+print("Hello")
+print(60)
+print("my name is rhea","i do athletics")
+print("my name is rhea\n","i do athletics") # \n means new line 
+
+
+print("hello i am", end=" ")
+print(17,"years old")
+
+print("hello " *10)
---
+++ L2_variables.py
@@ -0,0 +1,12 @@
+name="rhea"
+print(name)
+
+sentence="hello today is wednesday"
+print(sentence)
+
+num1=2
+num2=10
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
